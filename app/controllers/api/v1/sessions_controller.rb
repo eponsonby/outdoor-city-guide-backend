018505bd469a147d291eac276a2cf05f4623456f
Erin Ponsonby <erinponsonby@gmail.com>
2020-02-18 14:36:59 -0500

    Add cookie middleware

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::SessionsController < ApplicationController
     
     def create
-        @user = User.find_by(username: params[:session][:username]
-            if @user && @user.authenticate(params[:session][:pasword])
+        @user = User.find_by(username: params[:session][:username])
+            if @user && @user.authenticate(params[:session][:password])
                 session[:user_id] = @user.id
                 render json: @user
             else
@@ -10,8 +10,15 @@ class Api::V1::SessionsController < ApplicationController
                     error: "Invalid password"
                 }
             end
-
-        byebug
     end
 
+    def get_current_user
+        if logged_in?
+            render json: current_user
+        else
+            render json: {
+                error: "No one is logged in"
+            }
+        end
+    end
 end

79e88c94b616862d9a1e77b09716a2abdceb4694
Erin Ponsonby <erinponsonby@gmail.com>
2020-03-29 13:53:28 -0400

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'http://localhost:3000', 'http://localhost:3001'
-    origins '*'
+    origins 'https://the-trailhead-api.herokuapp.com/'
 
     resource '*',
       headers: :any,

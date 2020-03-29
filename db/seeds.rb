7e68762889326dd5f02fe0e1b84086a6c38007ca
Erin Ponsonby <erinponsonby@gmail.com>
2020-03-29 14:08:09 -0400

    Configure database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create( username: "erin", password: "password")
+User.create(username: "erin", password: "password")
 
 City.create(
     name: "Boulder",

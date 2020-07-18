4076cac8f9ccbb56e951ea893bc7d2fb1be29a96
Erin Ponsonby <erinponsonby@gmail.com>
2020-07-18 18:52:52 -0400

    Update origin url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://thetrailhead.herokuapp.com", "http://localhost:3000"
+    origins "thetrailhead.erinponsonby.com", "http://localhost:3000"
 
     resource '*',
       credentials: true,

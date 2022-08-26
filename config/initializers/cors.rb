156c3ccbaae650a7ae5df382429baed76526bac5
Erin Ponsonby <erinponsonby@gmail.com>
2022-08-26 11:36:47 -0600

    Update cors allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "thetrailhead.erinponsonby.co", "http://localhost:3000"
+    origins "thetrailhead.erinponsonby.co", "http://localhost:3000", "https://thetrailhead.herokuapp.com/"
 
     resource '*',
       credentials: true,

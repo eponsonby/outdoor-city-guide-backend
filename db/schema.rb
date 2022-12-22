e1d7422d8a133c79602333d4f8922884e4add5cf
Erin Ponsonby <erinponsonby@gmail.com>
2022-12-22 16:15:27 -0500

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -2,24 +2,23 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_22_154141) do
-
+ActiveRecord::Schema[7.0].define(version: 2020_03_22_154141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "campgrounds", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "park_id"
     t.integer "city_id"
   end
@@ -27,8 +26,8 @@ ActiveRecord::Schema.define(version: 2020_03_22_154141) do
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.string "state"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "climbing_gyms", force: :cascade do |t|
@@ -37,16 +36,16 @@ ActiveRecord::Schema.define(version: 2020_03_22_154141) do
     t.string "street"
     t.string "state"
     t.string "zip"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "city_id"
     t.string "url"
   end
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "city_id"
   end
@@ -56,8 +55,8 @@ ActiveRecord::Schema.define(version: 2020_03_22_154141) do
     t.string "street"
     t.string "state"
     t.string "zip"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "city_id"
     t.string "url"
   end
@@ -65,16 +64,16 @@ ActiveRecord::Schema.define(version: 2020_03_22_154141) do
   create_table "parks", force: :cascade do |t|
     t.string "name"
     t.string "state"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "city_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email"
   end
 
